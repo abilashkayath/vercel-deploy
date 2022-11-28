b6dc2bd480affba38b4b5c9a4d4b688cfef1fcf7
amenon <amenon@boostmyshop.in>
2022-11-28 16:02:30 +0530

    check

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started !
+          Checking !
           {/* <code className={styles.code}>pages/index.js</code> */}
         </p>
 
